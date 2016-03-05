a2704e5c463147a957ae0f55d4ee9a4be873bfae
Franck <franck.cotton@gmail.com>
2016-03-05 11:39:16 +0100

    Changed 'uri' to 'retrievalQuery' in the CodeListSpecification element

--- src/main/java/fr/insee/pogues/mock/CodeListFactory.java
+++ src/main/java/fr/insee/pogues/mock/CodeListFactory.java
@@ -48,7 +48,7 @@ public class CodeListFactory {
 		logger.debug("Creating code list specification " + specId);
 
 		codeListSpecification.setId(specId);
-		codeListSpecification.setUri("http://id.insee.fr/codes/" + specId.toLowerCase());
+		codeListSpecification.setRetrievalQuery("http://id.insee.fr/codes/" + specId.toLowerCase());
 
 		return codeListSpecification;
 
